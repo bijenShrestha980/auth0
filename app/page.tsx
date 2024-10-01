c5a2943229f3d5d47aff67de9c043c34f9c4f0bd
bijenShrestha980 <s.bijen93@gmail.com>
2024-10-01 14:18:50 +0545

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,35 @@
 import Image from "next/image";
+import { getSession } from "@auth0/nextjs-auth0";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getSession();
+  const user = session?.user;
+  const accessToken = session?.accessToken;
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="flex gap-4">
+        <a href="/api/auth/login">Login</a>
+        <a href="/api/auth/logout">Logout</a>
+      </div>
+      <p className="text-primary"></p>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
+        {/* <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
           priority
-        />
+        /> */}
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            {user ? `Hello, ${user.name}!` : "You are not logged  in."}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          {accessToken && <li>Your access token : ${accessToken}</li>}
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
